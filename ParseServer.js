bb13a6265efcd2361f67ea37511886f4857da3c9
devazeem <45028292+devazeem@users.noreply.github.com>
2019-01-27 21:59:51 +0500

    Update ParseServer.js

--- ParseServer.js
+++ ParseServer.js
@@ -6,19 +6,19 @@ var app = express();
 
 var api = new ParseServer({
 
-  databaseURI: process.env.DATABASE_URI,
+  databaseURI: process.env.mongodb://azeem:12345azeem@ds031661.mlab.com:31661/azeem-app,
 
-  appId: process.env.APP_ID,
+  appId: process.env.APPLICATION_ID,
 
   masterKey: process.env.MASTER_KEY,
 
-  serverURL: process.env.SERVER_URL,
+  serverURL: process.env.http://localhost:1337/parse,
 
 });
 
 app.use('/parse', api);
 
-app.listen(process.env.PORT || 5000, function() {
+app.listen(process.env.PORT || 1337, function() {
 
   console.log('parse-server-example running on port ' + process.env.PORT);
 
